dc385ed0621dab751c4b4c8e5e41668ddab2e604
Kevin <Kevin@Kevins-MacBook-Pro.local>
2015-10-29 16:19:46 -0500

    onload function

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,14 @@
 window.onload = function(){
   //alert('hey hey hey');
   statusReport('hey HEY HEY!!!!');
+
+  var username = prompt('what is your name?');
+  var mission = prompt('what is your mission');
+
+  var contentContainer = document.getElementById('content');
+
+  contentContainer.innerHTML = username + ' is on a mission to ' + mission;
+
 }
 
 //without this files will load out of order
