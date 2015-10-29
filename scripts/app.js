da3b82dd47149d089b1179f8de28baccee887df7
Kevin <Kevin@Kevins-MacBook-Pro.local>
2015-10-29 16:06:52 -0500

    add status report

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+// hey hey hey
+
+window.onload = function(){
+  //alert('hey hey hey');
+  statusReport('hey HEY HEY!!!!');
+}
+
+//without this files will load out of order
+
+function statusReport(message){
+  console.log(message);
+  return message;
+}
